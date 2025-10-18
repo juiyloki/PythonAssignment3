20d034ca553e9d7c09ade50bc0cc6b10f0f69e55
MateuszGnat <mateusz.gnat1@wp.pl>
2025-10-18 22:37:19 +0200

    Add files via upload
    Functions to create and read CSV files. Both functions are called with a directory path. If Dane.csv already exists in the directory, it is overwritten with a new file.

---
+++ make_read_csv.py
@@ -0,0 +1,26 @@
+from csv import DictReader
+from pathlib import Path
+import random
+
+#checks if file and returns time if model = 'A'
+def read_csv(path: Path):
+    csv_file = path / 'Dane.csv'
+
+    if csv_file.exists():
+        with open(csv_file, 'r') as csv_file:
+            reader = DictReader(csv_file, delimiter=';')
+            row = next(reader)
+
+            if row['Model'].strip() == 'A':
+                return int(row['Czas'].rstrip('s'))
+    return 0
+
+def make_csv(path: Path):
+    mlist = ['A', 'B', 'C']
+
+    model = random.choice(mlist)
+    score = random.randint(0, 1000)
+    time = random.randint(0, 1000)
+    csv_file = path / 'Dane.csv'
+
+    csv_file.write_text(f'Model; Wynik; Czas;\n{model}; {score} ; {time}s;')

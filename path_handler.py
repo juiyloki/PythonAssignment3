56dd962b93a7cc5adad5ee35e9ecc406d9d43262
Oskar Rowicki <60814347+orowicki@users.noreply.github.com>
2025-10-19 17:15:39 +0200

    Fix creating dirs, add formatted names for dirs

--- path_handler.py
+++ path_handler.py
@@ -3,12 +3,35 @@ from make_read_csv import read_csv
 from make_read_csv import make_csv
 
 
+def format_day(day: str) -> str:
+    formatted_days = {
+        "pn": "poniedziałek",
+        "wt": "wtorek",
+        "śr": "środa",
+        "czw": "czwartek",
+        "pt": "piątek",
+        "sb": "sobota",
+        "nd": "niedziela",
+    }
+    return formatted_days[day]
+
+
+def format_time(time: str) -> str:
+    formatted_times = {"r": "rano", "w": "wieczorem"}
+    return formatted_times[time]
+
+
+def create_path(month: str, day: str, time: str) -> Path:
+    path = Path.cwd() / month.capitalize() / format_day(day) / format_time(time)
+    return path
+
+
 # Takes lists of preformatted strings containing directory names,
 # creates paths to directories and generates a csv file in each one.
 #
 # example usage:
-# `generate_csv_files(["Styczeń", "Luty"], [["wtorek", "środa"], ["piątek"]],
-#                ["rano", "wieczorem"])`
+# `generate_csv_files(["styczeń", "luty"], [["wt", "śr"], ["pt"]],
+#                ["r", "w"])`
 def generate_csv_files(
     months: list[str],
     ranges_of_days: list[list[str]],
@@ -18,36 +41,41 @@ def generate_csv_files(
 
     for i in range(len(months)):
         for day in ranges_of_days[i]:
-            time = times_of_days[time_index] if i < len(times_of_days) else "rano"
+            time = (
+                times_of_days[time_index] if time_index < len(times_of_days) else "r"
+            )
             time_index += 1
 
-            path = Path.cwd() / months[i] / day / time
+            path = create_path(months[i], day, time)
 
-            path.parent.mkdir(parents=True, exist_ok=True)
+            path.mkdir(parents=True, exist_ok=True)
             make_csv(path)
 
+
 # Takes lists of preformatted strings containing directory names,
-# constructs paths to files in specified directories, 
+# constructs paths to files in specified directories,
 # returns the sum of times in the A column from each file.
 #
 # example usage:
-# `A_time = read_csv_files(["Styczeń", "Luty"], [["wtorek", "środa"], ["piątek"]], 
-#                          ["rano", "wieczorem"])`
+# `A_time = read_csv_files(["styczeń", "luty"], [["wt", "śr"], ["pt"]],
+#                          ["r", "w"])`
 def read_csv_files(
     months: list[str],
     ranges_of_days: list[list[str]],
     times_of_days: list[str],
-) -> int:
+) -> None:
     sum_of_times_in_A = 0
     time_index = 0
 
     for i in range(len(months)):
         for day in ranges_of_days[i]:
-            time = times_of_days[time_index] if i < len(times_of_days) else "rano"
+            time = (
+                times_of_days[time_index] if time_index < len(times_of_days) else "r"
+            )
             time_index += 1
 
-            path = Path.cwd() / months[i] / day / time
+            path = create_path(months[i], day, time)
 
             sum_of_times_in_A += read_csv(path)
 
-    return sum_of_times_in_A
+    print("Suma czasów Modelu A wynosi:", sum_of_times_in_A)

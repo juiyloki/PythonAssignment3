90a01fb367036170f3ee6ae35a5bb740bb5e00a4
Oskar Rowicki <60814347+orowicki@users.noreply.github.com>
2025-10-19 15:02:15 +0200

    Rewrite the module for better funcionality
    Split process_paths into to functions, add summing times in `A` functionality.

--- path_handler.py
+++ path_handler.py
@@ -1,34 +1,45 @@
-import os
 from pathlib import Path
-from typing import Callable
 from make_read_csv import read_csv
 from make_read_csv import make_csv
 
 
 # Takes lists of preformatted strings containing directory names,
-# creates paths to directories and performs `action` on each path.
-# `action` can be either `generate_file` or `read_file`
+# creates paths to directories and generates a csv file in each one.
 #
 # example usage:
-# `process_paths(["Styczeń", "Luty"], [["wtorek", "środa"], ["piątek"]],
-#                ["rano", "wieczorem"], read_file)`
-def process_paths(
+# `generate_csv_files(["Styczeń", "Luty"], [["wtorek", "środa"], ["piątek"]],
+#                ["rano", "wieczorem"])`
+def generate_csv_files(
     months: list[str],
     ranges_of_days: list[list[str]],
     times_of_days: list[str],
-    action: Callable[[Path], None],
-):
+) -> None:
     for i in range(len(months)):
         for day in ranges_of_days[i]:
             time = times_of_days[i] if i < len(times_of_days) else "rano"
             path = Path.cwd() / months[i] / day / time
-            action(path)
 
+            path.parent.mkdir(parents=True, exist_ok=True)
+            make_csv(path)
 
-def generate_file(path: Path):
-    path.parent.mkdir(parents=True, exist_ok=True)
-    make_csv(path)
+# Takes lists of preformatted strings containing directory names,
+# constructs paths to files in specified directories, 
+# returns the sum of times in the A column from each file.
+#
+# example usage:
+# `A_time = read_csv_files(["Styczeń", "Luty"], [["wtorek", "środa"], ["piątek"]], 
+#                          ["rano", "wieczorem"])`
+def read_csv_files(
+    months: list[str],
+    ranges_of_days: list[list[str]],
+    times_of_days: list[str],
+) -> int:
+    sum_of_times_in_A = 0
+    for i in range(len(months)):
+        for day in ranges_of_days[i]:
+            time = times_of_days[i] if i < len(times_of_days) else "rano"
+            path = Path.cwd() / months[i] / day / time
 
+            sum_of_times_in_A += read_csv(path)
 
-def read_file(path: Path):
-    read_csv(path)
+    return sum_of_times_in_A
